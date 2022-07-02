ef23b4f3932393a2c73bd0d2851851ae9e62eb7c
smartcoder0926 <smartcoder0926@gmail.com>
2022-07-02 12:48:12 -0500

    update

--- programs/wallet-nft-minting/src/lib.rs
+++ programs/wallet-nft-minting/src/lib.rs
@@ -6,7 +6,7 @@ use anchor_spl::token;
 use anchor_spl::token::{MintTo, Token};
 use mpl_token_metadata::instruction::{create_master_edition_v3, create_metadata_accounts_v2};
 
-declare_id!("2oSArBTstkKKkhrPPTJC4PXSou3zUPPmTTemQJqYnjAc");
+declare_id!("BfkwLq6tGq2CFw24kQJK5ZxZfBMTnv8a4vKQq76pEpRk");
 pub mod constants {
     pub const MINTING_PDA_SEED: &[u8] = b"wallet_nft_minting";
 }
